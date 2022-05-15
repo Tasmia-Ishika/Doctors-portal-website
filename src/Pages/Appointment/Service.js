0970ca3b3f664e9b41c664a8b25ff1127fac1d4d
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-15 16:54:28 +0600

    login and signup page added

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -13,7 +13,7 @@ const Service = ({ service, setTreatment }) => {
                 }</p>
                 <p className='text-center font-bold'>{slots.length} {slots.length > 1 ? 'Slots' : 'Slot'} Available</p>
                 <div className="card-actions justify-center">
-                    <label for="booking-modal" disabled={slots.length === 0} onClick={() => setTreatment(service)} className="btn btn-secondary bg-gradient-to-r from-secondary to-primary text-white font-bold mt-2 btn-sm uppercase">Book Appointment</label>
+                    <label htmlFor="booking-modal" disabled={slots.length === 0} onClick={() => setTreatment(service)} className="btn btn-secondary bg-gradient-to-r from-secondary to-primary text-white font-bold mt-2 btn-sm uppercase">Book Appointment</label>
 
                 </div>
             </div>
