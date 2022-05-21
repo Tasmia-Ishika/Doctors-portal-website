e2e826522ba39b2f75f9efde921943dc83ba7fc3
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-05-21 13:23:40 +0600

    dashboard updated

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
+import AddDoctor from './Pages/Dashboard/AddDoctor';
+import ManageDoctors from './Pages/Dashboard/ManageDoctors';
 
 function App() {
   return (
@@ -35,6 +37,14 @@ function App() {
           <Route path="history" element={<MyHistory></MyHistory>}></Route>
           <Route path="users" element={<RequireAdmin>
             <Users></Users>
+          </RequireAdmin>}>
+          </Route>
+          <Route path="addDoctor" element={<RequireAdmin>
+          <AddDoctor></AddDoctor>
+          </RequireAdmin>}>
+          </Route>
+          <Route path="manageDoctor" element={<RequireAdmin>
+          <ManageDoctors></ManageDoctors>
           </RequireAdmin>}>
           </Route>
 
